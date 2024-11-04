f1407618a64041a94e91fd5f6d98180da895e6bb
hiiamcailyn <caiyntran@gmail.com>
2024-11-04 10:04:39 -0800

    hi

--- Main.java
+++ Main.java
@@ -4,6 +4,13 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    while (x>-1)
+    {
+      System.out.println("Please print a positive number");
+      x = sc.nextInt();
+    }
   }
 }
+// if they give me positive numbers add it up, but if they give me -1 print sum
