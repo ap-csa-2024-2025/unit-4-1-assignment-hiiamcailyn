c22c00d45ffadd6de9b05e456b762ef6e447953f
hiiamcailyn <caiyntran@gmail.com>
2024-11-04 10:16:36 -0800

    hi

--- Main.java
+++ Main.java
@@ -5,11 +5,20 @@ public class Main
   public static void main(String[] args)
   {
     Scanner sc = new Scanner(System.in);
-    int x = sc.nextInt();
-    while (x>-1)
+    int sum = 0; 
+
+    // int sum = 0;
+    // int user1 = 2;
+    // int user2 = 12;
+    // int user3 = 3;
+    // int user6 = 
+    int input = 0; 
+    while (!(input == -1)) /// code not stopped yet 
     {
-      System.out.println("Please print a positive number");
-      x = sc.nextInt();
+      System.out.println("Please print a number");
+      input = sc.nextInt();
+      sum +=input; 
+      System.out.println("You gave" + sum);
     }
   }
 }
